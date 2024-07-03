b772f9f6741c5b5993e27498d16900496e30770b
bandalgomsu <q63530@naver.com>
2024-07-03 10:52:35 +0900

    feat : IP 요청 개수 제한 기능 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,43 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { VerificationModule } from './verification/verification.module';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { Redis } from 'ioredis';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
+
+import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, AuthModule, VerificationModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    VerificationModule,
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        storage: new ThrottlerStorageRedisService(
+          new Redis({
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+          }),
+        ),
+        throttlers: [
+          {
+            name: 'throttler',
+            ttl: seconds(60),
+            limit: 100,
+          },
+        ],
+      }),
+    }),
+  ],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
