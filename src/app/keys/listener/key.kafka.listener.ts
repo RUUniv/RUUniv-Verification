ad8e275cfb02682b3904ea1b1131ce69f78031b0
bandalgomsu <q63530@naver.com>
2024-08-22 20:48:13 +0900

    feat : 보상 트랜잭션 로직 추가

--- src/app/keys/listener/key.kafka.listener.ts
+++ src/app/keys/listener/key.kafka.listener.ts
@@ -4,25 +4,43 @@ import { ClientKafka, EventPattern } from '@nestjs/microservices';
 import {
   KAFKA_TOPIC_CREATE_API_KEY,
   KAFKA_TOPIC_DELETE_API_KEY,
+  KAFKA_TOPIC_ROLLBACK_CREATE_API_KEY,
+  KAFKA_TOPIC_ROLLBACK_DELETE_API_KEY,
 } from 'src/infrastructure/kafka/kafka.topic';
 
 @Controller()
 export class KafkaKeyListener {
   constructor(
     private readonly keyService: KeyService,
-    @Inject('KAFKA') private readonly kafka: ClientKafka,
+    @Inject('KAFKA') private readonly kafkaProducer: ClientKafka,
   ) {}
   private readonly logger = new Logger(KafkaKeyListener.name);
 
   @EventPattern(KAFKA_TOPIC_CREATE_API_KEY)
-  async createApiKeyHandler(data: any) {
-    this.logger.log('Create Api Key Event Start');
-    this.keyService.createApiKey(data);
+  async createApiKeyHandler(payload: any) {
+    try {
+      this.logger.log('Create Api Key Event Start');
+      await this.keyService.createApiKey(payload.userId);
+    } catch (e) {
+      this.logger.log('Create Api Key Event Failure');
+      this.kafkaProducer.emit(
+        KAFKA_TOPIC_ROLLBACK_CREATE_API_KEY,
+        payload.userId,
+      );
+    }
   }
 
   @EventPattern(KAFKA_TOPIC_DELETE_API_KEY)
-  async deleteApiKeyHandler(data: any) {
-    this.logger.log('Delete Api Key Event Start');
-    this.keyService.deleteApiKey(data.userId, data.apiKey);
+  async deleteApiKeyHandler(payload: any) {
+    try {
+      this.logger.log('Delete Api Key Event Start');
+      await this.keyService.deleteApiKey(payload.userId, payload.apiKey);
+    } catch (e) {
+      this.logger.log('Delete Api Key Event Failure');
+      this.kafkaProducer.emit(
+        KAFKA_TOPIC_ROLLBACK_DELETE_API_KEY,
+        payload.userId,
+      );
+    }
   }
 }
