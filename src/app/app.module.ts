aec3b67325150c46f98a0f2bb97e3ccfff0b44f2
bandalgomsu <q63530@naver.com>
2024-08-24 23:11:37 +0900

    refactor : throttler deprecated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { VerificationModule } from './verification/verification.module';
-import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { Redis } from 'ioredis';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
-
-import { ConfigService } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
 import { KeyModule } from './keys/key.module';
 
 @Module({
   imports: [UserModule, AuthModule, VerificationModule, KeyModule],
   controllers: [AppController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
 })
 export class AppModule {}
